538e9e03fbf4e69e3175198ed25cf9848742d067
Nicolas Viennot <nicolas@viennot.biz>
2022-01-06 22:08:57 -0500

    Use symlinks for the svd files

--- svd/extract.sh
+++ svd/extract.sh
@@ -5,9 +5,9 @@
 # SPDX-License-Identifier: MIT OR Apache-2.0
 
 # Copy GD32 SVD files and give them lower-case names.
-cp vendor/GD32F1x0.svd gd32f130.svd
-cp vendor/GD32F1x0.svd gd32f150.svd
-cp vendor/GD32F1x0.svd gd32f170.svd
-cp vendor/GD32F1x0.svd gd32f190.svd
-cp vendor/GD32E230.svd gd32e230.svd
-cp vendor/GD32E231.svd gd32e231.svd
+ln -fs vendor/GD32F1x0.svd gd32f130.svd
+ln -fs vendor/GD32F1x0.svd gd32f150.svd
+ln -fs vendor/GD32F1x0.svd gd32f170.svd
+ln -fs vendor/GD32F1x0.svd gd32f190.svd
+ln -fs vendor/GD32E230.svd gd32e230.svd
+ln -fs vendor/GD32E231.svd gd32e231.svd
