a525c52ecd127031c014370a5b3de138dd255cb6
Nicolas Viennot <nicolas@viennot.biz>
2022-01-06 22:36:30 -0500

    Add the gd32f305, gd32f307 families of the gd32f0x series

--- svd/extract.sh
+++ svd/extract.sh
@@ -11,3 +11,6 @@ ln -fs vendor/GD32F1x0.svd gd32f170.svd
 ln -fs vendor/GD32F1x0.svd gd32f190.svd
 ln -fs vendor/GD32E230.svd gd32e230.svd
 ln -fs vendor/GD32E231.svd gd32e231.svd
+
+ln -fs vendor/GD32F30x_CL.svd gd32f305.svd
+ln -fs vendor/GD32F30x_CL.svd gd32f307.svd
